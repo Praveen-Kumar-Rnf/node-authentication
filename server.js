e70716839ca8262b7f4793df2edbc435cd2be01d
Praveen Kumar <praveen.kumar@rnf.tech>
2024-01-31 00:00:40 +0530

    update

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
+//Import Libraies Start
 const express = require("express");
-const app = express()
+//Import Libraies Start
 
+const app = express();
+
+//Routes Start
 app.get('/', (req, res) => {
   res.render("index.ejs")
-})
+});
+
+app.get('/login', (req, res) => {
+  res.render("login.ejs")
+});
+
+app.get('/register', (req, res) => {
+  res.render("register.ejs")
+});
+
+//Routes End
 
 app.listen(3000)
